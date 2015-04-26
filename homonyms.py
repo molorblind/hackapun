951d17f39f2310397f8792905e20914be8307556
Marc-Olivier Ricard <moricard@keatext.com>
2015-04-25 21:40:53 -0400

    add homonyms class

---
+++ homonyms.py
@@ -0,0 +1,7 @@
+class Homonyms:
+  def __init__(self):
+    self.homs = [h.split(',') for h in
+        open("data/homophones.csv").read().split('\n')]
+
+  def homonyms(self, word):
+    return [hs for hs in self.homs if any([h for h in hs if word in h])]
